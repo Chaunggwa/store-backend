e6594357e1a576c1e199276e6d9e5a1ba1480f17
Chaunggwa <nautical19thureinsoe@gmail.com>
2023-09-13 08:55:04 +0630

    Fix Columns mismatch in orders

--- app/(dashboard)/[storeId]/(routes)/orders/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/cell-action.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { BillboardColumn } from "./columns";
+import { OrdersColumn } from "./columns";
 import { Button } from "@/components/ui/button";
 import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
 import { toast } from "react-hot-toast";
@@ -11,7 +11,7 @@ import { useState } from "react";
 import AlertModal from "@/components/modals/alert-modals";
 
 interface CellActionProps {
-    data: BillboardColumn
+    data: OrdersColumn
 }
 export const CellAction: React.FC<CellActionProps> = ({
     data
